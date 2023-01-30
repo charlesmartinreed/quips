10fbc06f159551fe3f68a218ddb553ea7891ced2
Charles Martin Reed <charlesmartinreed@icloud.com>
2023-01-29 23:34:45 -0600

    Add FizzBuzz.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // 1: Palindromes!
 const checkIfPalindrome = (word) => {
-  // of course, you could always just use reverse here
   return word === [...word].reverse().join("");
+};
+
+const fizzy = (countLimit) => {
+  return Array(countLimit)
+    .fill(1)
+    .map((n, idx) => (n += idx))
+    .map((m) => {
+      if (m % 5 === 0 && m % 3 === 0) return "FizzBuzz";
+      if (m % 3 === 0) return "Fizz";
+      if (m % 5 === 0) return "Buzz";
+      return m;
+    });
 };
 
 const printAnswer = (fn, ...args) => {
@@ -11,5 +22,7 @@ const printAnswer = (fn, ...args) => {
   console.log("Result of", fn.name, "is", result);
 };
 
-printAnswer(checkIfPalindrome, "milk");
-printAnswer(checkIfPalindrome, "racecar");
+// printAnswer(checkIfPalindrome, "milk");
+// printAnswer(checkIfPalindrome, "racecar");
+
+printAnswer(fizzy, 50);
