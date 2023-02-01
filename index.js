8b2315b4a22de17b0ea022640fcd968d14b5ed98
Charles Martin Reed <charlesmartinreed@icloud.com>
2023-01-31 21:55:13 -0600

    Create fibonacci generator function (now... how to quipify it?)

--- index.js
+++ index.js
@@ -22,6 +22,35 @@ const fizzy = (countLimit) => {
     });
 };
 
+const fibby = (placeCount) => {
+  // return Array(1)
+  //   .fill([0, 1], 0, 1)
+  //   .map((val, index, arr) => {
+  //     for (let i = 0; i <= placeCount; i++) {
+  //       let newVal = val + arr[index + 1];
+  //       arr.push()
+  //     }
+  //     arr.push();
+  //     return arr;
+  // while (index < placeCount) {
+  //   console.log(val, arr[index]);
+  //   // return val + arr[index + 1];
+  // }
+  // });
+  let step = 1;
+  let values = [0, 1];
+  while (step < placeCount) {
+    step++;
+    values = [...values, fibGenerator(values).next().value];
+  }
+  return values;
+};
+
+function* fibGenerator(currenvalues) {
+  yield currenvalues[currenvalues.length - 1] +
+    currenvalues[currenvalues.length - 2];
+}
+
 const printAnswer = (fn, ...args) => {
   let params = args.length > 1 ? args : args[0];
   let result = fn(params);
@@ -33,4 +62,4 @@ const printAnswer = (fn, ...args) => {
 // printAnswer(checkIfPalindrome, "racecar");
 
 // printAnswer(fizzy, 50);
-// console.log([].fill());
+printAnswer(fibby, 10);
