d1a277197f3d0eb717abab79ac95eec35dad4649
Charles Martin Reed <charlesmartinreed@icloud.com>
2023-03-28 01:13:16 -0500

    (first working example of imgAttrParser)

--- index.js
+++ index.js
@@ -1362,8 +1362,12 @@ const urlParser = (addrStr) => {
 let testImgSrc = `<img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://miro.medium.com/v2/resize:fit:828/format:webp/1*a__3IyfVHcksJuJnrHDN8A.jpeg">`;
 
 const imgAttrParser = (imageToParse) => {
+  // return imageToParse.split(/[\W{1,}]+/i);
+
+  return imageToParse.replace(/(img|<|>|\s)/gi, "").split(/([\b{1,}]="|;)/i);
+  return imageToParse.split(/[\W{1,}]+/i);
+
   return imageToParse.split(/[\b{1,}="\b{1,};]+/i);
-  return imageToParse.split(/^\b$="+/gi);
 };
 
 console.log(imgAttrParser(testImgSrc));
